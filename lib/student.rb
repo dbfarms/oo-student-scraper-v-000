54b8aad0a2fc1e31d1f2d1827a790bccf4b297a1
Matthew Hunger <matt.hunger@gmail.com>
2017-12-07 20:42:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,28 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def new(student_profile)
-    @name = student_profile[:name]  
+    @name = student_profile[:name]
     @location = student_profile[:location]
-    
-  end 
+
+  end
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
