774f504ce472bb8f007a70d6514ef860f53672b1
Matthew Hunger <matt.hunger@gmail.com>
2017-12-07 20:43:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_profile[:name]
-    @location = student_profile[:location]
+    @name = student_hash[:name]
+    @location = student_hash[:location]
   end
 
   def self.create_from_collection(students_array)
