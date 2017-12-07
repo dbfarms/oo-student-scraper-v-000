dafc3450dbed298c92afe3b2c5d642a85b14f37b
Matthew Hunger <matt.hunger@gmail.com>
2017-12-07 20:56:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     #binding.pry
-    student.twitter = attributes_hash[:twitter]
+    @twitter = attributes_hash[:twitter]
 
   end
 
